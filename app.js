31fc7f81ce5e89b9a0701a86992ba3ddc11857b6
MarioBlasi <mblasi509@gmail.com>
2023-05-09 11:48:01 +0100

    app.js

---
+++ app.js
@@ -0,0 +1,42 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      tasks: [],
+      api_url: "getTask.php",
+      new_task: "",
+    };
+  },
+  methods: {
+    add_task() {
+      console.log("add new task to the list");
+
+      const data = {
+        new_task: this.new_task,
+      };
+      axios
+        .post("storeTasks.php", data, {
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((response) => {
+          console.log(response);
+          this.tasks = response.data;
+        })
+        .catch((error) => {
+          console.error(error.message);
+        });
+    },
+  },
+  mounted() {
+    axios
+      .get(this.api_url)
+      .then((response) => {
+        console.log(response);
+        this.tasks = response.data;
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  },
+}).mount("#app");
---
+++ getTask.php
@@ -0,0 +1,10 @@
+<?php
+
+$tasks_json_string = file_get_contents('tasks.json');
+
+
+$tasks_string = file_get_contents('file_path');
+
+echo new_task_json_string;
+
+
